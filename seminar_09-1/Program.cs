4f42a74869f90433d13008ad4db3c44503b702d8
gmakc-094423 <gmakc-094423@yandex.ru>
2022-05-21 17:48:49 +0300

    Домашнее задание по 9 семинару.

---
+++ seminar_09-1/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.Clear();
+
+Console.WriteLine($"\n");
+int n = InputNumbers("Введите n: ");
+
+PrintNumber(n);
+
+void PrintNumber(int n) 
+{
+  if (n < 0) return;
+  PrintNumber(n - 1);
+  Console.Write(n + " ");
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+Console.WriteLine();
+
+while (true)
+{
+int m = InputNumbers("Введите m: ");
+int n1 = InputNumbers("Введите n: ");
+
+if (m > n1) 
+{
+  // n = m;
+  // m = n1;
+  // n1 = n;
+}
+else{ 
+PrintNumber1(m, n1);
+break;
+}
+}
+
+void PrintNumber1(int m, int n1) 
+{
+  if (n1 < m) return;
+  PrintNumber1(m, n1 - 1);
+  Console.Write(n1 + " ");
+}
---
+++ seminar_09-2/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.Clear();
+
+Console.WriteLine($"\n");
+int n = InputNumbers("Введите n: ");
+
+int m = SumNumber(n);
+
+int SumNumber(int n) 
+{
+  if (n > 0) return n % 10 + SumNumber(n / 10);
+  else return 0;
+}
+Console.WriteLine($"\n {m}");
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.WriteLine($"");
+double a = InputNumbersDouble("Введите A: ");
+double b = InputNumbersDouble("Введите В: ");
+
+double result = Degree(a, b); 
+Console.WriteLine($"\n {result}");
+
+double Degree(double a, double b)
+{
+  // Console.Write($"a={a}, b={b};   ");
+  if (b > 1) return a * Degree(a, b-1);
+  // Console.Write(a + " ");
+  return a;
+}
+
+double InputNumbersDouble(string input) 
+{
+  Console.Write(input);
+  double output = Convert.ToDouble(Console.ReadLine());
+  return output;
+}
